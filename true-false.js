fee2204afa421f80493a5263270d111ae6cfa0d5
Md Hridoy Ahmed <95656155+mdredoyahmed@users.noreply.github.com>
2024-10-01 21:27:40 +0600

    add file

---
+++ true-false.js
@@ -0,0 +1,20 @@
+/* 
+true 
+any number (+ve or -ve ) will be true other then 0
+any string other then empty string 
+
+
+falsy
+1. false 
+2 . 0 
+3. '' (empty string )
+*/
+
+const z = 'false' ;
+if (z){
+    console.log('value of z id truthy');
+
+}
+else{
+    console.log('value if z is falsy');
+}
